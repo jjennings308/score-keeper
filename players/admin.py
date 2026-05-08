cb1656c5624c6b2cbbc4af39929a8212a651d03f
James Jennings <jjennings308@gmail.com>
2026-05-07 21:29:11 -0400

    Adjusted players admin.py for changes to model.py

--- players/admin.py
+++ players/admin.py
@@ -1,31 +1,57 @@
 from django.contrib import admin
-
-from .models import Player, Team
+from .models import ClaimToken, GroupMembership, Player, PlayerGroup, Team
 
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'nickname', 'user', 'created_at']
-    list_filter = ['created_at']
-    search_fields = ['name', 'nickname', 'user__username', 'user__email']
-    readonly_fields = ['created_at']
-    raw_id_fields = ['user']
+    list_display = ('display_name', 'name', 'nickname', 'email', 'user', 'created_by', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'nickname', 'email')
+    raw_id_fields = ('user', 'created_by')
+    readonly_fields = ('created_at', 'updated_at')
 
 
-class PlayerInline(admin.TabularInline):
-    model = Team.players.through
-    extra = 1
-    verbose_name = 'Player'
-    verbose_name_plural = 'Players'
+@admin.register(PlayerGroup)
+class PlayerGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'member_count', 'created_at')
+    list_filter = ('owner',)
+    search_fields = ('name', 'owner__username')
+    raw_id_fields = ('owner',)
+    readonly_fields = ('created_at', 'updated_at')
+
+    def member_count(self, obj):
+        return obj.memberships.count()
+    member_count.short_description = 'Members'
+
+
+@admin.register(GroupMembership)
+class GroupMembershipAdmin(admin.ModelAdmin):
+    list_display = ('player', 'group', 'joined_at')
+    list_filter = ('group',)
+    search_fields = ('player__name', 'group__name')
+    raw_id_fields = ('player', 'group')
+    readonly_fields = ('joined_at',)
+
+
+@admin.register(ClaimToken)
+class ClaimTokenAdmin(admin.ModelAdmin):
+    list_display = ('player', 'created_at', 'expires_at', 'used', 'is_valid')
+    list_filter = ('used',)
+    search_fields = ('player__name', 'player__email')
+    raw_id_fields = ('player',)
+    readonly_fields = ('token', 'created_at', 'expires_at', 'is_valid')
+
+    def is_valid(self, obj):
+        return obj.is_valid
+    is_valid.boolean = True
+    is_valid.short_description = 'Valid'
 
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ['name', 'player_count', 'created_at']
-    search_fields = ['name']
-    readonly_fields = ['created_at']
-    inlines = [PlayerInline]
-    exclude = ['players']
+    list_display = ('name', 'player_count', 'created_at')
+    search_fields = ('name',)
+    filter_horizontal = ('players',)
 
     def player_count(self, obj):
         return obj.players.count()
